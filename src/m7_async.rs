7f6f48c8fba24601c9c7e058a8ada38f774b4c23
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-11 09:39:54 +0530

    changed error defination

--- src/m7_async.rs
+++ src/m7_async.rs
@@ -1,8 +1,15 @@
-async fn my_async_call(url:&str)->Result<serde_json::Value, reqwest::Error>{
+use::std::io::{Error,ErrorKind};
 
-let response = reqwest::get(url).await?.json::<serde_json::Value>().await?;
 
-Ok(response)
+async fn my_async_call(url:&str)->Result<serde_json::Value,Error>{
+
+let response = reqwest::get(url).await.map_err(|_|Error::new(ErrorKind::Other,"Could not retrieve response") )?;
+
+
+ let json_response = response.json::<serde_json::Value>().await.map_err(|_|Error::new(ErrorKind::Other,"Could not decode to json") )?;
+
+  
+Ok(json_response)
 
 }
 
@@ -12,21 +19,23 @@ Ok(response)
 #[cfg(test)]
 mod tests{
 
+use ethers::types::Res;
+
 use super::*;
 
 #[tokio::test]
 async fn test_calls_async_fn(){
 let api_url = "https://cat-fact.herokuapp.com/facts/";
 
-let my_res = my_async_call(api_url).await;
+let my_res: Result<serde_json::Value,std::io::Error> = my_async_call(api_url).await;
 
 
 match my_res{
-  Ok(res) => {dbg!(res)} ,
+  Ok(r) => {dbg!(r);} ,
   Err(_)=>{
     panic!{
       "failed to make api request"
-    }
+    };
   }
 }
 }

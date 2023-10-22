52d129a83b3ac31bced35cdf0f410f291d05f759
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-22 12:59:24 +0530

    using crates within modules

--- src/math_funcs.rs
+++ src/math_funcs.rs
@@ -1,19 +1,26 @@
+fn some_fn(){
+    println!("Basic Math functions ")
+}
+
+
 mod maths{
 
     pub mod basic_math{
-        use super::printing;
+
 
         pub fn multiplication(num1: &i32,num2:&i32)->i32{
             let result = num1*num2;
             printing(&result);
             result
         } 
+        fn printing(num:&i32){
+            println!("the result is {}",num);
+            crate::math_funcs::some_fn()
+        }
+    
     }
 
-    pub fn printing(num:&i32){
-        println!("the result is {}",num)
-    }
-
+    
 }
 
 pub fn rect_area(length:&i32,width:&i32)->i32{

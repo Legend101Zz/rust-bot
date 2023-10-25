dbde7da3bde434ab0aa90ca21be5c0db792bdd8f
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-25 19:28:25 +0530

    creating a recursive enum using smart pointers

--- src/m10_smart_pointers.rs
+++ src/m10_smart_pointers.rs
@@ -1,4 +1,10 @@
 
+enum List {
+    Cons(i32,Box<List>),
+    Nil,
+}
+
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -11,6 +17,17 @@ mod test {
     let x  = 0.625;
     println!("Are the values equal ? {} ", x == *single_value);
     dbg!{*single_value};
+
+    let stack_var = 4;
+    let stack_ref = &stack_var;
+
+ 
+
+ use List::{Cons,Nil};
+
+ let list = Cons(1,Box::new(Cons(2,Box::new(Cons::new(3,Box::new(Nil))))));
+
+    let heap_var = Box::new(stack_var);
     
     }
 

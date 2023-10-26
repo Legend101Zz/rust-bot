1faa51801f96684236869bb488b6af4b0fd8b8ac
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-26 13:04:59 +0530

    created multiple ownwers of a list

--- src/m12_reference_counting_pointer.rs
+++ src/m12_reference_counting_pointer.rs
@@ -1,3 +1,13 @@
+use std::rc::Rc;
+
+
+enum List{
+    Cons (i32, Rc<List>),
+    Nil
+}
+
+
+use List::{Cons, Nil};
 
 #[cfg(test)]
 mod test {
@@ -6,7 +16,15 @@ mod test {
     #[test]
 
     fn test_rc_pointer() {
-      
+      let a = Rc::new(Cons(1,Rc::new(Cons(2,Rc::new(Nil)))));
+
+      println!("Count after creating a ={}", Rc::strong_count(&a));
+let b = Rc::new(Cons(3,Rc::clone(&a)));
+println!("Count after creating b ={}", Rc::strong_count(&a));
+let c = Rc::new(Cons(4,Rc::clone(&a)));
+println!("Count after creating c ={}", Rc::strong_count(&a));
+
+
     }
 }
 

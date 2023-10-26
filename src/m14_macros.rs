1a0940e13787cdc39f379b1f9faad56e095234f9
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-26 17:39:41 +0530

    repeating pattern in macros

--- src/m14_macros.rs
+++ src/m14_macros.rs
@@ -8,6 +8,18 @@ macro_rules! our_macro {
     };
 }
 
+
+macro_rules! string_concat {
+ 
+
+    ($($some_str:expr),*)=>{{
+        let mut temp_str = String::new();
+
+    $(temp_str.push_str($some_str);)*;
+    temp_str}
+    }
+}
+
 macro_rules! input {
     ($t :ty) => {{
         let mut n = String::new();
@@ -23,21 +35,30 @@ macro_rules! input {
 
 #[cfg(test)]
 mod test {
+    use std::ffi::c_long;
+
     use super::*;
 
     #[test]
 
     fn test_macro() {
-        our_macro!();
+        // our_macro!();
 
-        println!("{}", our_macro!(2, 2));
+        // println!("{}", our_macro!(2, 2));
 
-        println! {"Hello{}",2};
+        // println! {"Hello{}",2};
 
-        println!("Please enter a floating point number ");
+        // println!("Please enter a floating point number ");
 
-        let some_input = input!(f32);
+        // let some_input = input!(f32);
 
-        println!("Number entered is  : {}", some_input);
+        // println!("Number entered is  : {}", some_input);
+
+        let str_null = string_concat!();
+        let str_single = string_concat!("First");
+
+        let str_multiple = string_concat!("Firts","Second");
+
+        println!("{}",str_multiple)
     }
 }
